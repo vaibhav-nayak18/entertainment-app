712d0c5c885e99965946f98382542ebcd4c2fbba
vaibhav nayak <vaibhavnayak018@gmail.com>
2023-10-18 16:46:25 +0530

    initializing native app

---
+++ App.tsx
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text className={'text-red-300 bg-green-300'}></Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+export default App;
